2f8c58a70970f56ce085ac71807c38af18bfa4a9
avira2011 <forever.avira@gmail.com>
2024-09-15 16:41:21 +0200

    sustainable.py

--- sustainable.py
+++ sustainable.py
@@ -8,12 +8,21 @@ ITEMS = ["bag","bottle","chips","battery"]
 items = []
 background = Actor('bground')
 current_level = 1
+animations = []
+game_over = False
+START_SPEED = 10
 
-
+def display_message(i):
+    screen.draw.text(i,fontsize = 60, center = (400,325), color = "black")
+    
 def draw():
     background.draw()
-    for i in items:
-        i.draw()
+    if game_over:
+        display_message("GAME OVER, TRY AGAIN")
+    else:
+        for i in items:
+            i.draw()
+    
 
 
 def update():
@@ -25,6 +34,7 @@ def make_items(current_level):
     items_to_create = get_items_to_create(current_level)
     draw_items = create(items_to_create)
     layout_items(draw_items)
+    animates(draw_items)
     return draw_items
 
 def get_items_to_create(current_level):
@@ -50,6 +60,18 @@ def layout_items(items_to_layout):
         new_x_pos = (index + 1) * gap_size
         item.x = new_x_pos
 
+def animates(animations_to_create):
+    global animations
+    for i in animations_to_create:
+        duration = START_SPEED - current_level
+        i.anchor = ("center", "bottom")
+        animation = animate(i,duration = duration, on_finished = handle_game_over, y=HEIGHT)
+        animations.append(animation)
+
+
+def handle_game_over():
+    global game_over
+    game_over = True
 
 
 pgzrun.go()
